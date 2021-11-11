6d3ccf89342751c2e9cb511adebaed6c4e6a77e1
iucimea <eugeniogambari@gmail.com>
2021-11-11 01:03:28 +0100

    Update turing.py

--- turing.py
+++ turing.py
@@ -50,19 +50,46 @@ def simulation(u,v):
             
 def observe(u):
     
-    plt.imshow(u, cmap='binary')
     plt.title(i)
-    plt.show()
+    plt.imsave(str(i), u, cmap='binary')
+    
+    plt.imshow(u, cmap='binary')
+
     
     
 #%% 
 
-image_list = []
+# image_list = []
 
 for i in range(simulation_steps):
     u,v = simulation(u,v)
     if i % 10 == 0:
         observe(u)
-        image_list.append(observe(u))
+        # image_list.append(observe(u))
     
+#%%
 
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+hmpf = np.ones([4,4])
+hmpf[2][1] = 0
+imagelist = [ hmpf*i*255./19. for i in range(20) ]
+
+fig = plt.figure() # make figure
+
+# make axesimage object
+# the vmin and vmax here are very important to get the color map correct
+im = plt.imshow(imagelist[0], cmap=plt.get_cmap('jet'), vmin=0, vmax=255)
+
+# function to update figure
+def updatefig(j):
+    # set the data in the axesimage object
+    im.set_array(imagelist[j])
+    # return the artists set
+    return [im]
+# kick off the animation
+ani = animation.FuncAnimation(fig, updatefig, frames=range(20), 
+                              interval=50, blit=True)
+plt.show()
